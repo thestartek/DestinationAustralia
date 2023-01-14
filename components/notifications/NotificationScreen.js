432f0c8fcddd425e6f909c52591af586e1115062
TR Awasthi <thestartek@users.noreply.github.com>
2023-01-14 16:23:16 +1000

    expo push notificatins start

--- components/notifications/NotificationScreen.js
+++ components/notifications/NotificationScreen.js
@@ -1,75 +1,114 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-  Linking,
-} from "react-native";
-import React from "react";
-import * as WebBrowser from "expo-web-browser";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, Button, Platform } from "react-native";
 
-const NotificationScreen = () => {
-  const payLink = () => {
-    Linking.openURL("https://PayPal.Me/thestartek")
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  return (
-    <ScrollView>
-      <Text style={styles.headerText}>
-        Notifications are not available right now.
-      </Text>
-      <Text style={styles.contentText}>
-        We are working hard to make this feature available to our users. Please
-        come back later to see notifications.
-      </Text>
-      <Text style={styles.contentText}>
-        For now, you can support us to make this application the bast app for
-        Australia related contents by offering your support amount to the
-        following PayPal ID.
-      </Text>
-
-      <View style={{ justifyContent: "center", alignItems: "center" }}>
-        <Image
-          source={{
-            uri: "https://firebasestorage.googleapis.com/v0/b/journeytoaustralia-b21d4.appspot.com/o/confidential%2FPayPal_qrCode.png?alt=media&token=fec45c06-f199-4608-a189-4e38af3b44ee",
-          }}
-          style={{ height: 200, width: 200, marginTop: 20 }}
-        />
-        <TouchableOpacity onPress={payLink}>
-          <Text style={styles.payID}>PayPal.Me/thestartek</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
-  );
-};
-
-export default NotificationScreen;
-
-const styles = StyleSheet.create({
-  headerText: {
-    fontSize: 28,
-    marginHorizontal: 20,
-    marginTop: 30,
-    marginBottom: 15,
-  },
-  contentText: {
-    fontSize: 16,
-    marginVertical: 5,
-    marginHorizontal: 20,
-    lineHeight: 26,
-  },
-  payID: {
-    marginVertical: 20,
-    textAlign: "center",
-    color: "#1267E9",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
 });
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{" "}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{" "}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
+  );
+}
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  return token;
+}
