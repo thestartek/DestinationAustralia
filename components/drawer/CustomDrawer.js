918817b2554cef1be53c88b3e575592f9c4388ec
TR Awasthi <thestartek@users.noreply.github.com>
2023-01-14 18:08:26 +1000

    minor changes

--- components/drawer/CustomDrawer.js
+++ components/drawer/CustomDrawer.js
@@ -96,7 +96,7 @@ const CustomDrawer = (props) => {
           </TouchableOpacity>
         </View>
 
-        <Text style={{ marginBottom: 50 }}>Version 1.0.2</Text>
+        <Text style={{ marginBottom: 50 }}>Version 1.0.3</Text>
         <View style={{ height: 100 }}></View>
       </View>
 
