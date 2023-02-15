664d92cff9a0745e0a1be6d1f60c65e4fcfb153d
TR Awasthi <thestartek@users.noreply.github.com>
2023-02-15 20:26:17 +1000

    v 2.1.1

--- components/drawer/CustomDrawer.js
+++ components/drawer/CustomDrawer.js
@@ -104,7 +104,7 @@ const CustomDrawer = (props) => {
           </TouchableOpacity>
         </View>
 
-        <Text style={{ marginBottom: 20 }}>Version 2.1.0</Text>
+        <Text style={{ marginBottom: 20 }}>Version 2.1.1</Text>
         <View style={{ alignItems: "center", marginBottom: 40 }}>
           <BannerAd unitId={adUnitId} size={BannerAdSize.MEDIUM_RECTANGLE} />
         </View>
